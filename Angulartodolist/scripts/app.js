a7da84fef9521cc9160f9e25ac0f2b635ed3ede5
Moses Williams III <varenigmatictheprogrammer1@gmail.com>
2016-08-29 20:10:53 -0400

    ng-click
    implementation of ng-click

--- Angulartodolist/scripts/app.js
+++ Angulartodolist/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope) {
+	$scope.helloWorld = function() {
+		console.log("Hello there! This is the helloWorld controller function, in the mainCtrl");
+	};
+})
+
+.controller('promoCtrl', function($scope){
+  $scope.iAmMoz = function() {
+  	console.log("Hello, my name is Moses!");
+  };
+
+  $scope.helloWorld = function() {
+
+  	console.log("This is not mainCtrl");
+  }
+
+})
+.controller("imASibling", function($scope) {
+	$scope.foobar = 1234;
+
+	//enter in some other things!
+});;
